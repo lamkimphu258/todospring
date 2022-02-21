975d0bba45f524bbd803148aa0176b9ba1fe4959
lamkimphu258 <lamkimphu258@gmail.com>
2022-02-21 17:24:37 +0700

    Init project

---
+++ src/main/java/lkp/com/todospring/TodospringApplication.java
@@ -0,0 +1,13 @@
+package lkp.com.todospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lkp/com/todospring/TodospringApplicationTests.java
@@ -0,0 +1,13 @@
+package lkp.com.todospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
